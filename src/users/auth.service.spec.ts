a1f81199a328d898408329d998f39f3cf7587457
MohamedAdel0301 <mosoliman33@gmail.com>
2024-08-20 05:28:32 +0300

    added tests to sign in method

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,8 +1,7 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
-import { BadRequestException } from '@nestjs/common';
-import { User } from './user.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 let service: AuthService;
 let fakeUserService;
@@ -45,6 +44,20 @@ describe('AuthService', () => {
       Promise.resolve([{ id: 1, email: 'a', password: '1' }]);
     //fine will resolve with an item, this invalidates the sign up process
     await expect(service.signUp('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signIn('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException);
+  });
+
+  it('throws if an invalid password is provided', async () => {
+    fakeUserService.findOneEmail = () =>
+      Promise.resolve({ email: 'asdf@asdf.com', password: 'las.kdjf' });
+    //the stored data that you get back
+    await expect(service.signIn('asdf@asdf.com', 'passowrd')).rejects.toThrow(
       BadRequestException,
     );
   });
